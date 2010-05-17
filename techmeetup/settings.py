8bf2bd94d46346f49a49d3659285ae8f0ff7c9c6
Oli Kingshott <o.kingshott@sms.ed.ac.uk>
2010-05-17 23:25:41 +0100

    added 404, 500 templates, fixed imports for apache

--- techmeetup/settings.py
+++ techmeetup/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'techmeetup.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(TECHMEETUP_DIR, "templates"),
