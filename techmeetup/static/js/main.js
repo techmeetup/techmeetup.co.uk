0df32176baa77f499188412d93414c39cefea820
Dale Harvey <dale@arandomurl.com>
2010-06-25 13:17:11 +0100

    added sponsors logos

--- techmeetup/static/js/main.js
+++ techmeetup/static/js/main.js
@@ -1,55 +1,6 @@
 
 
-/*
- * JavaScript Pretty Date
- * Copyright (c) 2008 John Resig (jquery.com)
- * Licensed under the MIT license.
- */
-
-// Takes an ISO time and returns a string representing how
-// long ago the date represents.
-function prettyDate(time){
-	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
-		diff = (((new Date()).getTime() - date.getTime()) / 1000),
-		day_diff = Math.floor(diff / 86400);
-			
-	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-		return;
-			
-	return day_diff == 0 && (
-			diff < 60 && "just now" ||
-			diff < 120 && "1 minute ago" ||
-			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-			diff < 7200 && "1 hour ago" ||
-			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		day_diff == 1 && "Yesterday" ||
-		day_diff < 7 && day_diff + " days ago" ||
-		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
-}
-
-// If jQuery is included in the page, adds a jQuery plugin to handle it as well
-if ( typeof jQuery != "undefined" ) {
-	jQuery.fn.prettyDate = function(){
-		return this.each(function(){
-			var date = prettyDate(this.title);
-			if ( date )
-				jQuery(this).text( date );
-		});
-	};
-}
-
-function autolink(s) {
-    var hlink = /\s(ht|f)tp:\/\/([^ \,\;\:\!\)\(\"\'\<\>\f\n\r\t\v])+/g;
-    return (s.replace (hlink, function ($0,$1,$2) {
-        s = $0.substring(1,$0.length); 
-        // remove trailing dots, if any
-        while (s.length>0 && s.charAt(s.length-1)=='.') 
-            s=s.substring(0,s.length-1);
-        // add hlink
-        return " " + s.link(s); 
-    }));
-}
-
-
-
-
+$(".fadehover").hover(
+    function () { $(this).fadeTo("fast", 1.0); },
+    function () { $(this).fadeTo("fast", 0.3); }
+);
