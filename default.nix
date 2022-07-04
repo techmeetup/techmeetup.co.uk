3a6bc01f965d25ba496b2e93902b11828afe0d9d
silky <noonsilk+-noonsilk@gmail.com>
2022-07-04 08:20:32 +0100

    nix build/env

---
+++ default.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> { };
+
+let jekyll_env = bundlerEnv rec {
+    name = "jekyll_env";
+    inherit ruby;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in
+  stdenv.mkDerivation rec {
+    name = "techmeetup.co.uk";
+    buildInputs = [ jekyll_env bundler ruby ];
+  }
+
